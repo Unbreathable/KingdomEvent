768ad9a5c86b2729c64d8a413fdff94a4975f069
Unbreathable <70802809+Unbreathable@users.noreply.github.com>
2026-09-03 09:00:06 +0200

    Initial commit

---
+++ src/main/java/com/liphium/kingdom/KingdomEvent.java
@@ -0,0 +1,16 @@
+package com.liphium.kingdom;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class KingdomEvent extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
